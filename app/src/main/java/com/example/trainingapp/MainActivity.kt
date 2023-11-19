dc9aafdcad92607d4c9d62657d76f0d7603acfc1
Dmitriy Kolomiets <koldim14kol@gmail.com>
2023-11-19 15:04:02 +0300

    Added a raw start screen

--- app/src/main/java/com/example/trainingapp/MainActivity.kt
+++ app/src/main/java/com/example/trainingapp/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.trainingapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.material3.Button
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.trainingapp.ui.theme.TrainingAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +27,47 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TwoButtons()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TwoButtons() {
+    Box(
+        contentAlignment = Alignment.Center
+    ){
+        Button(
+            onClick = { /* Действие для кнопки по центру */ },
+            modifier = Modifier.padding(8.dp)
+
+        ) {
+            Text("Регистрация")
+        }
+    }
+    Box(
+        contentAlignment = Alignment.BottomEnd
+    ){
+        Button(
+            onClick = { /* Действие для кнопки по центру */ },
+            modifier = Modifier.padding(8.dp)
+
+        ) {
+            Text("Пропустить")
+        }
+    }
+
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TrainingAppTheme {
-        Greeting("Android")
+        TwoButtons()
     }
 }
